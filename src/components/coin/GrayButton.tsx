8cfbe0aac3ec9ec0be9ac1f6d995d5632d856957
kanbei <kanbei0605@gmail.com>
2023-03-29 20:39:27 +0900

    updated bitcoin page

--- src/components/coin/GrayButton.tsx
+++ src/components/coin/GrayButton.tsx
@@ -1,15 +1,19 @@
 export interface IButton {
   text: string;
   active?: boolean;
+  svg?: string;
 }
 
-const GrayButton: React.FC<IButton> = (props = { text: "", active: false }) => {
+const GrayButton: React.FC<IButton> = (
+  props = { svg: "", text: "", active: false }
+) => {
   return (
     <div
       className={`${
-        props.active ? "bg-gray-700 text-white" : "bg-gray-300 text-gray-600"
-      } hover:bg-gray-600 rounded-xl px-4`}
+        props.active ? "bg-[#575757] text-white" : "bg-[#D9D9D9] text-[#7D7D7D]"
+      } hover:bg-gray-300 rounded-xl px-4 hover:cursor-pointer flex`}
     >
+      {props.svg ? <img src={props.svg} alt="svg" /> : ""}
       <span>{props.text}</span>
     </div>
   );
--- src/layouts/Bitcoin/index.tsx
+++ src/layouts/Bitcoin/index.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import GrayButton from "../../components/coin/GrayButton";
 import BitcoinSVG from "../../assets/svg/bitcoin.svg";
-import { WatchlistIcon } from "../../assets/icons";
+import { WatchlistIcon, ArrowUpIcon } from "../../assets/icons";
 
 const BitcoinPage: React.FC = () => {
   return (
@@ -29,8 +29,8 @@ const BitcoinPage: React.FC = () => {
           Bitcoin
         </NavLink>
       </div>
-      <div className="grid grid-cols-3">
-        <div className="col-1">
+      <div className="grid grid-cols-3 gap-x-8">
+        <div className="col-span-1">
           <div className="flex items-center my-4">
             <img className="w-10 mr-2" src={BitcoinSVG} alt="bitcoin" />
             <span className="font-bold text-lg mr-2">Bitcoin</span>
@@ -55,7 +55,77 @@ const BitcoinPage: React.FC = () => {
             <span className="text-blue-500 hover:cursor-pointer">View All</span>
           </div>
         </div>
-        <div className="col-2">2</div>
+        <div className="col-span-2">
+          <div className="font-bold flex items-center mb-6">
+            <span className="text-[70px] mr-2">$28,013.46</span>
+            <span className="px-2 py-3 text-white bg-green-500 rounded-2xl flex items-center text-2xl">
+              <ArrowUpIcon
+                className="mt-2"
+                color="white"
+                width={15}
+                height={15}
+              />
+              0.02%
+            </span>
+          </div>
+          <div className="grid grid-cols-4">
+            <div className="flex-col">
+              <span className="text-[#7D7D7D]">Market Cap</span>
+              <br />
+              <span className="font-bold">$535,170,972,845</span>
+              <span className="flex items-center text-green-500">
+                <ArrowUpIcon
+                  className="mt-2 mr-1"
+                  color="green"
+                  width={15}
+                  height={15}
+                />
+                0.02%
+              </span>
+            </div>
+            <div className="flex-col">
+              <span className="text-[#7D7D7D]">Fully Diluted Market Cap</span>
+              <br />
+              <span className="font-bold">$535,170,972,845</span>
+              <span className="flex items-center text-green-500">
+                <ArrowUpIcon
+                  className="mt-2 mr-1"
+                  color="green"
+                  width={15}
+                  height={15}
+                />
+                0.02%
+              </span>
+            </div>
+            <div className="flex-col">
+              <span className="text-[#7D7D7D]">Volume</span>
+              <br />
+              <span className="font-bold">$25,170,972,845</span>
+              <span className="flex items-center text-green-500">
+                <ArrowUpIcon
+                  className="mt-2 mr-1"
+                  color="green"
+                  width={15}
+                  height={15}
+                />
+                0.02%
+              </span>
+            </div>
+            <div className="flex-col">
+              <span className="text-[#7D7D7D]">Circulating Supply</span>
+              <br />
+              <span className="font-bold">19,327,200 BTC</span>
+              <span className="flex items-center text-green-500">
+                <div className="w-full bg-gray-400 rounded-full h-2.5 dark:bg-gray-700">
+                  <div
+                    className="bg-gray-700 h-2.5 rounded-full"
+                    style={{ width: "60%" }}
+                  ></div>
+                </div>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
