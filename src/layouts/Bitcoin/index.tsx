3b8737ee69166d79a4ed84c8a5f89320d00b5eca
kanbei <kanbei0605@gmail.com>
2023-03-30 01:28:40 +0900

    updated chart

--- src/layouts/Bitcoin/index.tsx
+++ src/layouts/Bitcoin/index.tsx
@@ -3,6 +3,30 @@ import GrayButton from "../../components/coin/GrayButton";
 import BitcoinSVG from "../../assets/svg/bitcoin.svg";
 import { WatchlistIcon, ArrowUpIcon } from "../../assets/icons";
 import BitTabComponent from "../../components/Bitcoin/TabComponent";
+import ChartComponent from "../../components/Bitcoin/ChartComponent";
+
+const initialData = [
+  { time: "2018-12-22", value: 32.51 },
+  { time: "2018-12-23", value: 31.11 },
+  { time: "2018-12-24", value: 27.02 },
+  { time: "2018-12-25", value: 27.32 },
+  { time: "2018-12-26", value: 25.17 },
+  { time: "2018-12-27", value: 28.89 },
+  { time: "2018-12-28", value: 25.46 },
+  { time: "2018-12-29", value: 23.92 },
+  { time: "2018-12-30", value: 22.68 },
+  { time: "2018-12-31", value: 28.67 },
+  { time: "2019-01-01", value: 25.68 },
+  { time: "2019-01-02", value: 24.67 },
+  { time: "2019-01-03", value: 26.68 },
+  { time: "2019-01-04", value: 28.67 },
+  { time: "2019-01-05", value: 23.68 },
+  { time: "2019-01-06", value: 25.67 },
+  { time: "2019-01-07", value: 30.68 },
+  { time: "2019-01-08", value: 32.67 },
+  { time: "2019-01-09", value: 34.68 },
+  { time: "2019-01-10", value: 40.67 },
+];
 
 const BitcoinPage: React.FC = () => {
   return (
@@ -137,7 +161,9 @@ const BitcoinPage: React.FC = () => {
         </span>
         <BitTabComponent />
       </div>
-      <div>Chart</div>
+      <div>
+        <ChartComponent data={initialData} />;
+      </div>
     </div>
   );
 };
